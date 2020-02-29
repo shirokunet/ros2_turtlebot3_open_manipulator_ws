4604d881cc7dd2e76c8e38d3cd14e20d0c981109
shirokunet <shirokunet@gmail.com>
2020-02-29 13:08:51 +0900

    change path

--- set_path.sh
+++ set_path.sh
@@ -4,4 +4,4 @@
 export ROS_DOMAIN_ID=30 #TURTLEBOT3
 export TB3_MODEL=burger
 export TURTLEBOT3_MODEL=burger
-export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:~/github/ros2_turtlebot3_ws/src/turtlebot3/turtlebot3_simulations/turtlebot3_gazebo/models
+export GAZEBO_MODEL_PATH=./src/turtlebot3/turtlebot3_simulations/turtlebot3_gazebo/models
